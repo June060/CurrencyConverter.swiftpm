e83bb009f202fc76114d4c1cbb06a53dc39c8fbd
James Howard <jamh885@gmail.com>
2023-05-12 14:27:07 -0500

    omg i fixed it it WORKS!!!  IT NO OVERWIRTE

--- setCreator.swift
+++ setCreator.swift
@@ -3,8 +3,9 @@ import SwiftUI
 struct setCreator: View {
     @State var term1:String = ""
     @State var definition1:String = ""
-    @State var pair:[pairs] = []
+    @State var pair:Data = (UserDefaults.standard.data(forKey: "pair") ?? Data([]))
     var body: some View {
+        
         VStack{
             Text("Your Terms")
             HStack{
@@ -18,20 +19,32 @@ struct setCreator: View {
             .textFieldStyle(.roundedBorder)
             .padding()
             .onSubmit {
-                pair.append(pairs(term2: term1, definition2: definition1))
-
-                if let data = try? PropertyListEncoder().encode(pair) {
+                if let data = UserDefaults.standard.data(forKey: "pair") {
+                    var pair = try! PropertyListDecoder().decode([pairs].self, from: data)
+                    pair.append(pairs(term2: term1, definition2: definition1))
+                    
+                    
+                    
+                    if let data = try? PropertyListEncoder().encode(pair) {
                         UserDefaults.standard.set(data, forKey: "pair")
                     }
-                
+                }
             }
             Button(action: {
                 
-                pair.append(pairs(term2: term1, definition2: definition1))
-                if let data = try? PropertyListEncoder().encode(pair) {
+                if let data = UserDefaults.standard.data(forKey: "pair") {
+                    var pair = try! PropertyListDecoder().decode([pairs].self, from: data)
+                    pair.append(pairs(term2: term1, definition2: definition1))
+                    print("Here")
+                    print("Here")
+                    print("Here")
+                    print("Here")
+                    print(pair)
+                    
+                    if let data = try? PropertyListEncoder().encode(pair) {
                         UserDefaults.standard.set(data, forKey: "pair")
                     }
-
+                }
             }, label: {
                 Image(systemName: "plus.circle")
             })
@@ -41,6 +54,6 @@ struct setCreator: View {
         .onAppear(perform: {
             term1 = UserDefaults.standard.string(forKey: "term") ?? ""
             definition1 = UserDefaults.standard.string(forKey: "definition") ?? ""
-        }) 
+        })
     }
 }
